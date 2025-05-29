3983f1d62f125707b4bcc567cd2d404be21c5179
John Metzger <56820659+jometzg@users.noreply.github.com>
2025-05-29 16:29:17 +0100

    Update main.tf

--- iac/azure/level4/storage/main.tf
+++ iac/azure/level4/storage/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-container-storage"
+  name     = "gds-rg"
   location = "UK South"
 }
 
@@ -14,7 +14,7 @@ resource "azurerm_user_assigned_identity" "uami" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "blobstorageacct01"
+  name                     = "jjblobstorageacct01"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
