de64e675d42e57b943acb3ae56d34abbfecfecca
John Metzger <56820659+jometzg@users.noreply.github.com>
2025-05-29 16:23:52 +0100

    Create main.tf

---
+++ iac/azure/level4/storage/main.tf
@@ -0,0 +1,65 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-container-storage"
+  location = "UK South"
+}
+
+resource "azurerm_user_assigned_identity" "uami" {
+  name                = "uami-container-access"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "blobstorageacct01"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "mycontainer"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_role_assignment" "blob_reader" {
+  scope                = azurerm_storage_account.storage.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azurerm_user_assigned_identity.uami.principal_id
+}
+
+resource "azurerm_container_group" "aci" {
+  name                = "mycontainergroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.uami.id]
+  }
+
+  container {
+    name   = "mycontainer"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  ip_address_type = "public"
+  dns_name_label  = "aci-demo-${random_id.dns.hex}"
+}
+
+resource "random_id" "dns" {
+  byte_length = 4
+}
